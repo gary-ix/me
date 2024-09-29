d9cbb047b4ca4c10f5abd3f917fa6942690b2b89
theprogrammergary <theprogrammergary@gmail.com>
2024-09-29 14:43:54 -0400

    update default page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1 class="bg-background-start text-xl">
-	Welcome to SvelteKit Static Site Template
-</h1>
+<div class="flex flex-col items-center justify-center min-h-screen">
+	<h1 class="bg-background-start text-3xl text-center mb-8">
+		What flavor of the site would you like to see?
+	</h1>
+
+	<a 
+		href="https://svelte.theprogrammergary.com" 
+		class="bg-blue-500 hover:bg-blue-600 text-white text-xl font-bold py-4 px-8 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+	>
+		Svelte
+	</a>
+</div>
