ad2e8eaa2e0a74f673f80e877790bab8a384d396
theprogrammergary <theprogrammergary@gmail.com>
2024-10-11 14:24:50 -0400

    linting

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-	import { Button as ButtonPrimitive } from "bits-ui";
-	import { type Events, type Props, buttonVariants } from "./index.js";
-	import { cn } from "$lib/utils.js";
+	import { cn } from '$lib/utils.js'
+	import { Button as ButtonPrimitive } from 'bits-ui'
 
-	type $$Props = Props;
-	type $$Events = Events;
+	import { type Events, type Props, buttonVariants } from './index.js'
 
-	let className: $$Props["class"] = undefined;
-	export let variant: $$Props["variant"] = "default";
-	export let size: $$Props["size"] = "default";
-	export let builders: $$Props["builders"] = [];
-	export { className as class };
+	type $$Props = Props
+	type $$Events = Events
+
+	let className: $$Props['class'] = undefined
+	export let variant: $$Props['variant'] = 'default'
+	export let size: $$Props['size'] = 'default'
+	export let builders: $$Props['builders'] = []
+	export { className as class }
 </script>
 
 <ButtonPrimitive.Root
-	{builders}
-	class={cn(buttonVariants({ variant, size, className }))}
+	builders={builders}
+	class={cn(buttonVariants({ className, size, variant }))}
 	type="button"
 	{...$$restProps}
 	on:click
