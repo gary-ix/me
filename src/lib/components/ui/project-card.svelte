5bf6ef2cba8aed04927004bc59ade8a12fc64d38
theprogrammergary <theprogrammergary@gmail.com>
2025-04-20 15:40:57 -0500

    make project card similar to skill card

--- src/lib/components/ui/project-card.svelte
+++ src/lib/components/ui/project-card.svelte
@@ -9,18 +9,21 @@
 </script>
 
 <a
-	class=" group overflow-hidden rounded-xl
-	bg-background transition-all duration-100 hover:brightness-90 dark:hover:brightness-150 md:p-4"
+	class="
+	shadow-even-accent group overflow-hidden rounded-xl bg-background-start transition-all duration-100
+	hover:scale-105 hover:shadow-accentHard-alt3 hover:brightness-90 dark:hover:brightness-150 md:shadow-none
+	"
 	href={route}
 >
 	<div class="flex flex-col md:flex-row">
 		<div class="w-full p-3 pt-6 md:w-1/3">
 			{#if imageUrl}
-				<div class="aspect-rectangle w-full overflow-hidden rounded-3xl">
+				<div
+					class="aspect-rectangle w-full overflow-hidden rounded-3xl bg-background p-6 md:p-2"
+				>
 					<img
 						alt={title}
-						class="h-full w-full rounded-3xl object-cover transition-transform
-						 duration-200 group-hover:border-2 group-hover:border-accentHard-alt3"
+						class="h-full w-full rounded-xl object-cover transition-transform duration-200"
 						src={imageUrl}
 					/>
 				</div>
